d876fe23974ef55177b50acc74f96861ea47a68f
davidgeorgebell <bell.dave93@gmail.com>
2021-03-31 10:04:12 +0800

    Problem 1 additonal method

--- 001.js
+++ 001.js
@@ -2,22 +2,26 @@
 
 // Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
 
-// const multiplesOf3And5 = number => {
-//   let sum = 0;
-
-//   for (let i = 0; i < number; i++) {
-//     if (i % 3 === 0 || i % 5 === 0) {
-//       sum += i;
-//     }
-//   }
-//   return sum;
-// };
 const multiplesOf3And5 = number => {
+  let sum = 0;
+
+  for (let i = 0; i < number; i++) {
+    if (i % 3 === 0 || i % 5 === 0) {
+      sum += i;
+    }
+  }
+  return sum;
+};
+// output = 233168
+
+// Alternative using modern JavaScript methods
+
+const multiplesOf3And5WithMethods = number => {
   return Array(number)
     .fill(0)
-    .map((value, index) => index + 1)
+    .map((value, index) => index)
     .filter(value => value % 3 === 0 || value % 5 === 0)
     .reduce((sum, num) => sum + num);
 };
-console.log(multiplesOf3And5(1000));
+multiplesOf3And5WithMethods(1000);
 // output = 233168
